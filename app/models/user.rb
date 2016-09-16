2252d9295c5f0a7e661d091d6987f33ec4e0ce77
lizdenhup <lizdenhup@gmail.com>
2016-09-15 21:00:02 -0500

    added #slug and #find_by_slug to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ActiveRecord::Base
   has_secure_password
   
   def slug 
-
+    username.downcase.gsub(" ","-")
   end 
 
   def find_by_slug(slug)
-
+    User.all.find { |user| user.slug == slug }
   end 
+  
 end 
