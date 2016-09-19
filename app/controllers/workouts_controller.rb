e7c417b51e8228cb83ecd2c70311dab8d69a15eb
lizdenhup <lizdenhup@gmail.com>
2016-09-18 21:53:09 -0500

    added GET route for editing a workout, as well as edit_workout.erb view

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -30,6 +30,18 @@ class WorkoutsController < ApplicationController
     end 
   end 
 
+  get '/workouts/:id/edit' do 
+    if logged_in? 
+      @workout = Workout.find_by_id(params[:id])
+      erb :'/workouts/edit_workout'
+      #TODO figure out how to resubmit forms with
+      #the proper radio button checked (ie the 
+      #radio button that was active upon initial
+      #submission)
+    end 
+  end 
+
+
   post '/workouts' do 
     if logged_in?
       @workout = Workout.new(time: params[:time], date: params[:date], workout_type: params[:workout_type], distance: params[:distance], unit: params[:unit], notes: params[:notes], user_id: current_user.id)
