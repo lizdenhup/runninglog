8ebaa92d894780a276eb7026833148ae4b9f54f1
lizdenhup <lizdenhup@gmail.com>
2016-09-28 21:51:50 -0500

    styled users index page and edit form

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,8 +1,8 @@
-require 'enumerated_attribute'
-
 class Workout < ActiveRecord::Base
 #  enumerated_attribute :workout_type, %w(cycling running)
 #  enumerated_attribute :unit, %w(miles km)
 
   belongs_to :user
+
+
 end
